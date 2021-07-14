b4faa43033664bb3735981140ad91b5e0ebee8ea
Michael <rnfnoontothree@gmail.com>
2021-07-14 11:17:38 -0500

    Added logic for Sovyrn RBTC yield opportunity.

---
+++ opportunities/rbtc.md
@@ -0,0 +1,8 @@
+---
+name: 'RSK - Sovryn - rBTC'
+api: '/api/yield/rbtc'
+link: 'https://live.sovryn.app/lend'
+credentials: ''
+category: 'DeFi'
+json_path_rate: '$.nextSupplyInterestRate'
+---
--- pages/api/yield/[...slug].js
+++ pages/api/yield/[...slug].js
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import ibBTCabi from '/abis/ibBTCABI.json';
+import rBTCabi from '/abis/rBTCABI.json';
 
 export default async function handler(req, res) {
     const { slug } = req.query
@@ -9,20 +10,25 @@ export default async function handler(req, res) {
             const weekOldBlock = dayOldBlock * 7
             const apyFromLastWeek = await fetchIbbtcApyFromTimestamp(weekOldBlock)
             res.status(200).json({ apyFromLastWeek: apyFromLastWeek })
+            break
+        case 'rbtc':
+            const nextSupplyInterestRate = await fetchRbtcApy()
+            res.status(200).json({ nextSupplyInterestRate: nextSupplyInterestRate })
+            break
+        default:
+            res.status(404).send()
     }
-    res.status(404).send()
 }
 
 async function fetchIbbtcApyFromTimestamp(timestamp) {
-    const secondsPerYear = ethers.BigNumber.from(31536000)
-    const provider = new ethers.providers.AlchemyProvider()
-    const contractAddress = '0xc4e15973e6ff2a35cc804c2cf9d2a1b817a8b40f'
-    const contract = new ethers.Contract(contractAddress, ibBTCabi, provider)
-    const currentPPS = await contract.pricePerShare()
-    const fixedCurrentPPS = ethers.FixedNumber.from(currentPPS);
-    const currentBlock = await provider.getBlockNumber() - 50
-
     try {
+        const secondsPerYear = ethers.BigNumber.from(31536000)
+        const provider = new ethers.providers.AlchemyProvider()
+        const contractAddress = '0xc4e15973e6ff2a35cc804c2cf9d2a1b817a8b40f'
+        const contract = new ethers.Contract(contractAddress, ibBTCabi, provider)
+        const currentPPS = await contract.pricePerShare()
+        const fixedCurrentPPS = ethers.FixedNumber.from(currentPPS);
+        const currentBlock = await provider.getBlockNumber() - 50
         const targetBlock = currentBlock - Math.floor(timestamp / 15)
         const oldPPS = await contract.pricePerShare({ blockTag: targetBlock })
         const fixedOldPPS = ethers.FixedNumber.from(oldPPS);
@@ -31,6 +37,22 @@ async function fetchIbbtcApyFromTimestamp(timestamp) {
         return growthPerSecond.round(6).toString()
     } catch (error) {
         console.error(`Error while getting ibBTC APY from block ${currentBlock - timestamp}: ${error}`)
+        return null
+    }
+}
+
+async function fetchRbtcApy() {
+    // This logic is based off of https://github.com/DistributedCollective/Sovryn-frontend > src/app/components/NextSupplyInterestRate/index.tsx
+    try {
+        const provider = new ethers.providers.JsonRpcProvider('https://public-node.rsk.co')
+        const contractAddress = '0xa9DcDC63eaBb8a2b6f39D7fF9429d88340044a7A'
+        const contract = new ethers.Contract(contractAddress, rBTCabi, provider)
+        // TODO: "1" below represents the amount of rBTC that will be lent.  When connecting a user's wallet, use their balance instead of this arbitrary number.
+        const apy = await contract.nextSupplyInterestRate('1')
+        const fixedAPY = ethers.FixedNumber.from(apy).divUnsafe(ethers.FixedNumber.from(ethers.BigNumber.from("10").pow(20)))
+        return fixedAPY.round(6).toString()
+    } catch (error) {
+        console.error(`Error while getting rBTC APY: ${error}`)
         return null
     }
 }
